b4d31a3363d1130136fc65fdbbfad7dbac8cd499
eason <eason.li@trendy-global.com>
2018-05-17 12:00:02 +0800

    测试提交

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #### 个人react后台管理系统
+
+##### 技术站
+`React、react-router、redux、express`
+
+
+##### 参考资料
+react-router: [博客](https://zhuanlan.zhihu.com/p/28585911)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import {Router, Route, Switch} from 'react-router';
+import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HomePage = () => <h1>Home Page</h1>;
+const UserPage = () => <h1>User Page</h1>;
+const NotFoundPage = () => <h1>Not Found Page</h1>;
+
+
+const PrimaryLayout = () =>
+  <div className="primary-layout">
+    <header>Our react router 4 App</header>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/user">user</Link>
+      </li>
+    </ul>
+    <main>
+      <Switch>
+        <Route path="/" exact component={HomePage}/>
+        <Route path="/user" component={UserPage}/>
+        <Route path="/404" component={NotFoundPage}/>
+        <Redirect to="/404"/>
+      </Switch>
+    </main>
+  </div>;
+
+const MyApp = () =>
+  <BrowserRouter>
+    <PrimaryLayout/>
+  </BrowserRouter>;
+
+
+ReactDOM.render(<MyApp/>, document.getElementById('root'));
 registerServiceWorker();
